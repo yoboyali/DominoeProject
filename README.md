64ef1e15b49c486b279b7766bd795d7bfe4ef8c3
yoboyali <alihamdyeltomy@gmail.com>
2025-11-24 16:09:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DominoeProject
+
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    static void main() {
+
+        IO.println(String.format("Hello and welcome!"));
+
+        for (int i = 1; i <= 5; i++) {
+
+            IO.println("i = " + i);
+        }
+    }
+}
